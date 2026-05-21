5ca81a442f69644a0e64590003c8ccd1fbb4e1cc
Felipe Carvalho <carvalhotkd@gmail.com>
2026-05-21 16:47:21 -0400

    banco de dados com sqlalchemy e migrations com alembic

---
+++ tests/test_db.py
@@ -0,0 +1,24 @@
+from fastapi_zero.models import User
+from sqlalchemy import select
+from dataclasses import asdict
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='test', email='test@test.com', password='secret'
+        )
+
+        session.add(new_user)
+        session.commit()
+
+        user = session.scalar(select(User).where(User.username == 'test'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'test',
+        'email': 'test@test.com',
+        'password': 'secret',
+        'created_at': time,
+        'update_at': None,
+    }
